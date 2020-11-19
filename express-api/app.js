d474217c0ea6bb3c9157201e5cca8878fd196310
fallensteed <scott@hubertart.com>
2020-11-19 14:02:40 -0700

    updated APIs

--- express-api/app.js
+++ express-api/app.js
@@ -8,46 +8,119 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 // Database Connection
-import db from "./db";
+const Pool = require('pg').Pool;
+const db = new Pool({
+    user: 'ceph',
+    host: 'localhost', //Update host as needed
+    database: 'cephs_citchen',
+    password: 'ceph',
+    port: 5400 //Update port as needed
+})
+
+app.get("", (req, res) => {
+    res.send("Connected");
+});
 
 // GET items  ---- /shoppinglist/items
-app.get("/shoppinglist/items", (req, res) => {
-        db.query('SELECT * FROM tbl_items', (db_err, db_res) => {
-            if (db_err) {
-                throw db_err;
-            }
-            res.status(200).json(db_res.rows);
-        });
+app.get("/items", (req, res) => {
+    db.query("SELECT * FROM tbl_items", (db_err, db_res) => {
+        if (db_err) {
+            throw db_err;
+        }
+        res.status(200).json(db_res.rows);
+    });
 });
 
 // GET item by id  ---- /shoppinglist/items?id=#
-app.get("/shoppinglist/item", (req, res) => {
+app.get("/item", (req, res) => {
     if (req.query.id) {
-        db.query('SELECT * FROM tbl_items WHERE item_id = $1', [req.query.id], (db_err, db_res) => {
-            if (db_err) {
-                throw db_err;
+        db.query(
+            "SELECT * FROM tbl_items WHERE item_id = $1",
+            [req.query.id],
+            (db_err, db_res) => {
+                if (db_err) {
+                    throw db_err;
+                }
+                res.status(200).json(db_res.rows);
             }
-            res.status(200).json(db_res.rows);
-        });
+        );
     } else if (req.query.search) {
-        let strSearch = '%' +  decodeURIComponent(req.query.search) + '%';
-        db.query('SELECT * FROM tbl_items WHERE item_name LIKE ')
+        let strSearch = "%" + decodeURIComponent(req.query.search) + "%";
+        db.query("SELECT * FROM tbl_items WHERE item_name LIKE ");
     }
 });
 
 // POST item to items
-app.post("/shoppinglist/item", (req, res) => {
-
-})
-
-
+app.post("/item", (req, res) => {
+    let result;
+    const itemDetails = req.body;
+    if (itemDetails.name && itemDetails.categoryID) {
+        db.query(
+            "INSERT INTO tbl_items (item_name, category_id) VALUES ($1, $2)",
+            [itemDetails.name, itemDetails.categoryID],
+            (db_err, db_res) => {
+                if (db_err) {
+                    throw db_err;
+                }
+            }
+        );
+        result = {
+            status: "success",
+            message: "The message was successfully sent",
+        };
+    } else {
+        result = {
+            status: "failed",
+            message: "The message was not sent",
+        };
+        res.status(400);
+    }
+    res.json(result);
+});
 
 // GET shoppinglist
+app.get("/shoppinglist", (req, res) => {
+    db.query("SELECT * FROM tbl_shoppinglist", (db_err, db_res) => {
+        if (db_err) {
+            throw db_err;
+        }
+        res.status(200).json(db_res.rows);
+    });
+});
+
 
 // POST item to shoppinglist
+app.post("/shoppinglist/item", (req, res) => {
+    let result;
+    const itemDetails = req.body;
+    if (itemDetails.name && itemDetails.categoryID) {
+        db.query(
+            "INSERT INTO tbl_items (item_name, category_id) VALUES ($1, $2)",
+            [itemDetails.name, itemDetails.categoryID],
+            (db_err, db_res) => {
+                if (db_err) {
+                    throw db_err;
+                }
+            }
+        );
+        result = {
+            status: "success",
+            message: "The item was successfully added",
+        };
+    } else {
+        result = {
+            status: "failed",
+            message: "The item was not added",
+        };
+        res.status(400);
+    }
+    res.json(result);
+});
 
 // DELETE item from shoppinglist
 
 // DELETE item from shoppinglist
 
 // POST item to pantry
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- express-api/db.js
+++ express-api/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
-const pool = new Pool({
+const db = new Pool({
     user: 'ceph',
     host: 'localhost', //Update host as needed
     database: 'cephs_citchen',
-    password: '',
-    port: 64847 //Update port as needed
+    password: 'ceph',
+    port: 5400 //Update port as needed
 })
 
-export default pool;
+module.exports = { db };
